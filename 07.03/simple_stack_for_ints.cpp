869e9f1270931da5888999c55a66480636257e9c
yordan pulov <yordanpulov@gmail.com>
2014-03-07 22:16:36 +0200

    Add CPP simple stack for integers

---
+++ 07.03/simple_stack_for_ints.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+using namespace std;
+
+class Stack
+{
+
+public:
+
+	void push(int );
+	int pop();
+	Stack();
+	~Stack();
+
+private:
+
+	struct Node
+	{
+		int value;
+		Node *prev;
+	};
+
+	Node *top;
+	int count;
+
+};
+
+Stack::Stack()
+{
+	top=nullptr;
+	count=0;
+}
+
+void Stack::push(int element)
+{
+	Node *temp=new Node;
+	temp->value=element;
+	temp->prev=top;
+	count++;
+	top=temp;
+}
+int Stack::pop()
+{
+	if(count==0)
+	{
+		return -1;
+	}
+
+	int p= top->value;
+	Node *todelete=top;
+	top = top->prev;
+
+	delete todelete;
+	count--;
+	return p;
+}
+
+Stack::~Stack()
+{
+	int value;
+	do
+	{
+		value=this->pop();
+	}
+
+	while (value!=-1);
+
+}
+
+int main()
+{
+	Stack myS;
+	int value;
+	cin>>value;
+
+	while (value!=-1)
+	{
+		myS.push(value);
+		cin>>value;
+	}
+
+	int temp = myS.pop();
+
+	while(temp != -1)
+	{
+		cout<<temp<<" ";
+		temp = myS.pop();
+	}
+
+	system("pause");
+	return 0;
+}
+
